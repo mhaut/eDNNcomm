aae438d5f56c6b65c48d5fed6dc4e03b72b5b146
mhaut <mhaut@users.noreply.github.com>
2023-10-16 15:34:21 +0200

    add code

--- GAN/models.py
+++ GAN/models.py
@@ -9,26 +9,27 @@ class Generator(nn.Module):
 
         self.label_emb = nn.Embedding(n_classes, latent_dim)
         self.init_size = img_size // 4
-        self.l1 = nn.Sequential(nn.Linear(latent_dim, (128*k) * self.init_size ** 2))
+        self.k = k
+        self.l1 = nn.Sequential(nn.Linear(latent_dim, (128*self.k) * self.init_size ** 2))
 
         self.conv_blocks = nn.Sequential(
-            nn.BatchNorm2d(128*k),
+            nn.BatchNorm2d(128*self.k),
             nn.Upsample(scale_factor=2),
-            nn.Conv2d(128*k, 128*k, 3, stride=1, padding=1),
-            nn.BatchNorm2d(128*k, 0.8),
+            nn.Conv2d(128*self.k, 128*self.k, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128*self.k, 0.8),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Upsample(scale_factor=2),
-            nn.Conv2d(128*k, 64*k, 3, stride=1, padding=1),
-            nn.BatchNorm2d(64*k, 0.8),
+            nn.Conv2d(128*self.k, 64*self.k, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64*self.k, 0.8),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64*k, channels, 3, stride=1, padding=1),
+            nn.Conv2d(64*self.k, channels, 3, stride=1, padding=1),
             nn.Tanh(),
         )
 
     def forward(self, noise, labels):
         gen_input = torch.mul(self.label_emb(labels), noise)
         out = self.l1(gen_input)
-        out = out.view(out.shape[0], 128*k, self.init_size, self.init_size)
+        out = out.view(out.shape[0], 128*self.k, self.init_size, self.init_size)
         img = self.conv_blocks(out)
         return img
 
